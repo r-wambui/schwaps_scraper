4ca0807334f549393a746ce801ca5367fc7bd832
r-wambui <rose.wambui780@gmail.com>
2020-11-14 14:03:27 +0300

    Add proc file

--- scheduler.py
+++ scheduler.py
@@ -6,7 +6,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=10, minute=40)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=11, minute=5)
 
 def scheduled_job():
     subprocess.call('./scrape.sh', shell=True)
