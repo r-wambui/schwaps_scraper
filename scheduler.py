3c0990181df1e62919068b9e574946210215def5
r-wambui <rose.wambui780@gmail.com>
2020-11-14 15:15:35 +0300

    edit txt

--- scheduler.py
+++ scheduler.py
@@ -6,7 +6,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=12, minute=10)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=12, minute=20)
 
 def scheduled_job():
     subprocess.call('./scrape.sh', shell=True)
