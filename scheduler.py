e2e45560d58c7824c46f6cf1c172955091999e0b
r-wambui <rose.wambui780@gmail.com>
2020-11-16 16:30:21 +0300

    Edit time

--- scheduler.py
+++ scheduler.py
@@ -7,9 +7,9 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 def calendar():
-    subprocess.call('./scrape.sh', shell=True)
+    subprocess.call('bash scrape.sh', shell=True)
     logging.debug("Finished daily run")
 
-sched.add_job(calendar, 'cron', day_of_week='mon-fri', hour=13, minute=15)
+sched.add_job(calendar, 'cron', day_of_week='mon-fri', hour=13, minute=32)
     
 sched.start()
